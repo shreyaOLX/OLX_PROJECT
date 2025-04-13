8afc1460ac7a2c8eb6923133ebd59d4178efa0f9
Abhishek <abhishek.prasad@olx.in>
2025-04-13 22:44:05 +0530

    userController Class

---
+++ src/main/java/com/olx/inventories/controller/UserController.java
@@ -0,0 +1,26 @@
+
+package com.olx.inventories.controller;
+
+import com.olx.inventories.model.User;
+import com.olx.inventories.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+}
+
