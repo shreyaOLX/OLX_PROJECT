5cad1eee74569449ea16b9f3767bd4a617bf3648
AnuragKumar2704 <anurag.kumar@olx.in>
2025-04-13 14:17:35 +0530

    create file for inventory test cases

---
+++ src/test/java/com/olx/inventories/controller/InventoryControllerTest.java
@@ -0,0 +1,44 @@
+package com.olx.inventories.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getByPage() {
+    }
+
+    @Test
+    void createInventory() {
+    }
+
+    @Test
+    void updateInventory() {
+    }
+
+    @Test
+    void updateStatus() {
+    }
+
+    @Test
+    void updatePricing() {
+    }
+
+    @Test
+    void updateAttribute() {
+    }
+
+    @Test
+    void deleteInventory() {
+    }
+}
