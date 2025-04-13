6d1b26f1da371535e76795ddacb01ef8936ed497
Abhishek <abhishek.prasad@olx.in>
2025-04-13 21:47:34 +0530

    Create UserRepository

--- src/main/java/com/olx/inventories/repository/UserRepository.java
+++ src/main/java/com/olx/inventories/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.olx.inventories.repository;
 
-public class UserRepository {
+import com.olx.inventories.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
