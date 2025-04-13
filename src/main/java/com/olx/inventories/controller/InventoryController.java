f4ce9fffe6ce6a264eb24cf7a4640462c9476e3d
AnuragKumar2704 <anurag.kumar@olx.in>
2025-04-13 13:19:06 +0530

    add post mapping constraints

--- src/main/java/com/olx/inventories/controller/InventoryController.java
+++ src/main/java/com/olx/inventories/controller/InventoryController.java
@@ -1,12 +1,14 @@
 package com.olx.inventories.controller;
 
+import com.olx.inventories.model.Inventory;
 import com.olx.inventories.service.InventoryService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("/inventories")
@@ -24,5 +26,31 @@ public class InventoryController {
     @GetMapping("/{id}")
     public ResponseEntity<String> getById(@PathVariable Long id) {
         return inventoryService.get(id);
+    }
+
+    @Value("${inventory.items-per-page:10}")
+    private int itemOnEachPage;
+
+    @GetMapping("/items")
+    public ResponseEntity<Page<Inventory>> getByPage(@RequestParam(required = false) int pageNum) {
+        try {
+            Pageable pageable = PageRequest.of(pageNum, itemOnEachPage);
+            Page<Inventory> page = inventoryService.getPage(pageable);
+
+            if (page.isEmpty()) {
+                return ResponseEntity.notFound().build();
+            }
+
+            return ResponseEntity.ok(page);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
+    }
+
+    @PostMapping("/create")
+    private ResponseEntity<String> createInventory(@RequestBody Inventory item){
+
+        return inventoryService.create(item);
+
     }
 }
