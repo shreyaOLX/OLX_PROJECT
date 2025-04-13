3aa4beb6c979721bfd60c5acb4e0395a2230781b
AnuragKumar2704 <anurag.kumar@olx.in>
2025-04-13 13:40:43 +0530

    update get mapping constraints

--- src/main/java/com/olx/inventories/controller/InventoryController.java
+++ src/main/java/com/olx/inventories/controller/InventoryController.java
@@ -32,7 +32,7 @@ public class InventoryController {
     private int itemOnEachPage;
 
     @GetMapping("/items")
-    public ResponseEntity<Page<Inventory>> getByPage(@RequestParam(required = false) int pageNum) {
+    public ResponseEntity<Page<Inventory>> getByPage(@RequestParam(defaultValue = "0") int pageNum){
         try {
             Pageable pageable = PageRequest.of(pageNum, itemOnEachPage);
             Page<Inventory> page = inventoryService.getPage(pageable);
