c51ae4a595b00289933df19e301613008b1e2a81
shreyaOLX <shreya.shukla@olx.in>
2025-04-13 17:14:02 +0530

    write test case return null when inventory is valid

--- src/test/java/com/olx/inventories/util/InventoryValidatorTest.java
+++ src/test/java/com/olx/inventories/util/InventoryValidatorTest.java
@@ -1,6 +1,10 @@
 package com.olx.inventories.util;
 
+import com.olx.inventories.Enum.ItemType;
+import com.olx.inventories.model.Inventory;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +14,25 @@ class InventoryValidatorTest {
     void setUp() {
         inventoryValidator = new InventoryValidator();
     }
-
+    @Test
+    void returnNullWhenInventoryIsValid(){
+        Inventory inventory = new Inventory();
+        inventory.setType(ItemType.CAR.name());
+        inventory.setLocation("Delhi");
+        inventory.setCostPrice(800000L);
+        inventory.setSellingPrice(9000000L);
+        inventory.setAttribute("""
+                {
+                "VIN" : "UP38XX0001",
+                "make" : "Toyota",
+                "model" : "Fortuner",
+                "year" : "2023"
+                }
+                """);
+        inventory.setStatus("CREATED");
+        ResponseEntity<String> response = inventoryValidator.validate(inventory);
+        assertNull(response);
+    }
 
 
 }
